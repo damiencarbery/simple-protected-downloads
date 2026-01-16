c0845c770124282097d13917b8b1eee84507906a
Damien Carbery <daymobrew@yahoo.com>
2026-01-16 19:41:29 +0000

    Lots of tidying. Create an activation hook to flush rewrite rules; change uploads dir name; add JS alert() when download not permitted; move creation of uploads dir to a function; remove unused labels from CPT registration; delete uploaded file when post deleted; remove CSS colours.

--- simple-protected-downloads.php
+++ simple-protected-downloads.php
@@ -9,28 +9,20 @@ License: GPL v3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 Text Domain: simple-protected-downloads
 Domain Path: /languages
-Version: 0.1.20260114
+Version: 0.1.20260116
 */
 
 defined( 'ABSPATH' ) || exit;
 
 
-/*
-register_activation_hook( __FILE__, 'activate' );
-
-function activate() {
-    //this function will be called at activation, and then on every init as well:
-    SimpleProtectedDownloads::register_download_endpoint();
-
-    //run this at activation ONLY, AFTER setting the endpoint - needed for your endpoint to actually create a query_vars entry:
-    flush_rewrite_rules();
+// Force the rewrite rules to be regenerated after this plugin is actived.
+// Suggested at: https://developer.wordpress.org/reference/functions/flush_rewrite_rules/#comment-2645
+register_activation_hook( __FILE__, 'SimpleProtectedDownloads_activate' );
+register_deactivation_hook( __FILE__, 'SimpleProtectedDownloads_activate' );
+function SimpleProtectedDownloads_activate() {
+	// Force rewrite rules to be recreated at the right time
+	delete_option( 'rewrite_rules' );
 }
-
-
-register_deactivation_hook( __FILE__, 'deactivate' );
-function deactivate() {
-    flush_rewrite_rules();
-}*/
 
 
 class SimpleProtectedDownloads {
@@ -72,8 +64,6 @@ class SimpleProtectedDownloads {
 		// Download the requested file.
 		add_action( 'template_include', array( $this, 'download_file' ) );
 
-		// Add metabox to the CPT.
-		add_action( 'add_meta_boxes', array( $this, 'add_file_metabox' ) );
 		// Enable file uploads on the Edit Download admin page.
 		add_action( 'post_edit_form_tag', array( $this, 'enable_upload_support' ) );
 
@@ -86,12 +76,14 @@ class SimpleProtectedDownloads {
 		// Add the JS to allow copying of the download url to the clipboard.
 		add_action( 'in_admin_footer', array( $this, 'add_download_url_copying_js' ));
 
-// ToDo: Add code to delete the uploaded file when a post is deleted.
+		// Delete the uploaded file when a Download post is deleted.
+		add_action( 'before_delete_post', array( $this, 'delete_post' ), 10, 2 );
 	}
+
 
 	private function get_uploads_dir() {
 		if ( empty( $this->uploads_dir ) ) {
-			$this->uploads_dir = wp_get_upload_dir()['basedir']  . '/woocommerce_uploads/';
+			$this->uploads_dir = sprintf( '%s/%s/', wp_get_upload_dir()['basedir'], 'spd_uploads' );
 		}
 
 		return $this->uploads_dir;
@@ -103,46 +95,58 @@ class SimpleProtectedDownloads {
 	}
 
 
+	private function verify_uploads_dir_ready() {
+		if ( !file_exists( $this->get_uploads_dir() ) ) {
+			wp_mkdir_p( $this->get_uploads_dir() );
+		}
+
+		// Add .htaccess for protection.
+		$htaccess_file = $this->get_uploads_dir() . '/.htaccess';
+		if ( !file_exists( $htaccess_file ) ) {
+			file_put_contents( $htaccess_file, 'deny from all' );
+		}
+		// Add empty index.html for protection.
+		$index_file = $this->get_uploads_dir() . '/index.html';
+		if ( !file_exists( $index_file ) ) {
+			file_put_contents( $index_file, '' );
+		}
+	}
+
+
 	// Register the Download custom post type.
 	public function register_cpt() {
-// ToDo: Review all these labels - can some be removed?
 		$labels = array(
 			'name'                  => _x( 'Downloads', 'Post Type General Name', 'simple-protected-downloads' ),
 			'singular_name'         => _x( 'Download', 'Post Type Singular Name', 'simple-protected-downloads' ),
 			'menu_name'             => __( 'Downloads', 'simple-protected-downloads' ),
 			'name_admin_bar'        => __( 'Download', 'simple-protected-downloads' ),
-			'archives'              => __( 'Download Archives', 'simple-protected-downloads' ),
-			'attributes'            => __( 'Download Attributes', 'simple-protected-downloads' ),
-			'parent_item_colon'     => __( 'Parent download:', 'simple-protected-downloads' ),
+			//'archives'              => __( 'Download Archives', 'simple-protected-downloads' ),
+			//'attributes'            => __( 'Download Attributes', 'simple-protected-downloads' ),
+			//'parent_item_colon'     => __( 'Parent download:', 'simple-protected-downloads' ),
 			'all_items'             => __( 'All downloads', 'simple-protected-downloads' ),
 			'add_new_item'          => __( 'Add new download', 'simple-protected-downloads' ),
-			'add_new'               => __( 'Add download', 'simple-protected-downloads' ),
-			'new_item'              => __( 'New download', 'simple-protected-downloads' ),
-			'edit_item'             => __( 'Edit download', 'simple-protected-downloads' ),
-			'update_item'           => __( 'Update download', 'simple-protected-downloads' ),
-			'view_item'             => __( 'View download', 'simple-protected-downloads' ),
-			'view_items'            => __( 'View downloads', 'simple-protected-downloads' ),
-			'search_items'          => __( 'Search download', 'simple-protected-downloads' ),
+			//'add_new'               => __( 'Add download', 'simple-protected-downloads' ),
+			//'new_item'              => __( 'New download', 'simple-protected-downloads' ),
+			//'edit_item'             => __( 'Edit download', 'simple-protected-downloads' ),
+			//'update_item'           => __( 'Update download', 'simple-protected-downloads' ),
+			//'view_item'             => __( 'View download', 'simple-protected-downloads' ),
+			//'view_items'            => __( 'View downloads', 'simple-protected-downloads' ),
+			//'search_items'          => __( 'Search download', 'simple-protected-downloads' ),
 			'not_found'             => __( 'Not found', 'simple-protected-downloads' ),
 			'not_found_in_trash'    => __( 'Not found in Trash', 'simple-protected-downloads' ),
-			//'featured_image'        => __( 'Featured Image', 'simple-protected-downloads' ),
-			//'set_featured_image'    => __( 'Set featured image', 'simple-protected-downloads' ),
-			//'remove_featured_image' => __( 'Remove featured image', 'simple-protected-downloads' ),
-			//'use_featured_image'    => __( 'Use as featured image', 'simple-protected-downloads' ),
-			'insert_into_item'      => __( 'Insert into item', 'simple-protected-downloads' ),
-			'uploaded_to_this_item' => __( 'Uploaded to this download', 'simple-protected-downloads' ),
-			'items_list'            => __( 'Downloads list', 'simple-protected-downloads' ),
-			'items_list_navigation' => __( 'Downloads list navigation', 'simple-protected-downloads' ),
-			'filter_items_list'     => __( 'Filter downloads list', 'simple-protected-downloads' ),
+			//'insert_into_item'      => __( 'Insert into item', 'simple-protected-downloads' ),
+			//'uploaded_to_this_item' => __( 'Uploaded to this download', 'simple-protected-downloads' ),
+			//'items_list'            => __( 'Downloads list', 'simple-protected-downloads' ),
+			//'items_list_navigation' => __( 'Downloads list navigation', 'simple-protected-downloads' ),
+			//'filter_items_list'     => __( 'Filter downloads list', 'simple-protected-downloads' ),
 		);
 		$args = array(
 			'label'                 => __( 'Download', 'simple-protected-downloads' ),
 			'description'           => __( 'Protected downloads', 'simple-protected-downloads' ),
 			'labels'                => $labels,
 			'supports'              => array( 'title' ),
+			// ToDo: Decide whether to create custom categories.
 			'taxonomies'            => array( 'category' ),
-			'hierarchical'          => false,
-			'public'                => true,
 			'show_ui'               => true,
 			'show_in_menu'          => true,
 			'menu_position'         => 5,
@@ -150,12 +154,12 @@ class SimpleProtectedDownloads {
 			'show_in_admin_bar'     => true,
 			'show_in_nav_menus'     => false,
 			'can_export'            => true,
-			'has_archive'           => false,
 			'exclude_from_search'   => true,
 			'publicly_queryable'    => false,
 			'rewrite'               => false,
 			'capability_type'       => 'page',
 			'show_in_rest'          => false,
+			'register_meta_box_cb'  => array( $this, 'add_file_metabox' ),
 		);
 		register_post_type( $this->cpt_name, $args );
 	}
@@ -163,6 +167,18 @@ class SimpleProtectedDownloads {
 
 	public function register_download_endpoint() {
 		add_rewrite_endpoint( $this->download_url, EP_ROOT );
+	}
+
+
+	// Delete the uploaded file when A Download post is deleted.
+	public function delete_post( $post_id, $post ) {
+		$file_name = get_post_meta( $post_id, $this->meta_key, true );
+		if ( $file_name ) {
+			$file_path = $this->get_uploads_dir() . $file_name;
+			if ( file_exists( $file_path ) ) {
+				wp_delete_file( $file_path );
+			}
+		}
 	}
 
 
@@ -180,11 +196,8 @@ class SimpleProtectedDownloads {
 				if ( $user_can_download ) {
 					$file_name = get_post_meta( $post_id, $this->meta_key, true );
 					if ( $file_name ) {
-						//error_log( 'download_file $file_name: ' . $file_name );
 						$file_path = $this->get_uploads_dir() . $file_name;
 						if ( file_exists( $file_path ) && is_readable( $file_path ) ) {
-							//error_log( 'download_file $file_path: ' . $file_path );
-
 							header( 'X-Robots-Tag: noindex, nofollow', true );
 							header( 'Cache-Control: no-cache, must-revalidate, max-age=0, no-store, private' );
 							header( 'Content-Description: File Transfer', false );
@@ -200,6 +213,9 @@ class SimpleProtectedDownloads {
 
 							// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 							echo file_get_contents( $file_path );
+
+							do_action( 'spdownload_after_download', $post_id );
+
 							exit;
 						}
 					}
@@ -207,8 +223,24 @@ class SimpleProtectedDownloads {
 			}
 		}
 
+		add_action( 'wp_footer', array( $this, 'download_not_permitted_message' ) );
+
 		// Nothing matched our endpoint.
 		return $template;
+	}
+
+
+	// Display a JavaScript alert() when the user is not allowed to download the file.
+	// This could be triggered if the file does not exist but it is easier to have a generic message.
+	public function download_not_permitted_message() {
+		$plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . plugin_basename( __FILE__ ) );
+		$handle = $plugin_data['TextDomain'];
+		wp_register_script( $handle, '', null, $plugin_data['Version'], true );
+		wp_enqueue_script( $handle );
+		wp_add_inline_script( $handle, "window.addEventListener('DOMContentLoaded', function() {
+		alert( \"" . esc_html__( 'You do not have permission to download that file.', 'simple-protected-downloads' ) . "\" );
+		});"
+		);
 	}
 
 
@@ -224,11 +256,13 @@ class SimpleProtectedDownloads {
 		);
 	}
 
+
 	public function enable_upload_support( $post ) {
 		if ( $post->post_type === $this->cpt_name ) {
 			echo ' enctype="multipart/form-data"';
 		}
 	}
+
 
 	// Metabox callback function
 	function file_metabox_callback( $post ) {
@@ -257,6 +291,7 @@ class SimpleProtectedDownloads {
 		</div>
 		<?php
 	}
+
 
 	// Save metabox data
 	function save_file_metabox( $post_id ) {
@@ -288,17 +323,8 @@ class SimpleProtectedDownloads {
 
 		// Handle file upload
 		if ( isset( $_FILES['dcwdspd_file'] ) && isset( $_FILES['dcwdspd_file']['error'] ) && $_FILES['dcwdspd_file']['error'] == 0 ) {
-// ToDo: Move the dir and .htaccess creation to a separate function that can be called on plugin initialisation.
-			// Create upload directory if it doesn't exist
-			if ( !file_exists( $this->get_uploads_dir() ) ) {
-				wp_mkdir_p( $this->get_uploads_dir() );
-			}
-
-			// Add .htaccess for protection
-			$htaccess_file = $this->get_uploads_dir() . '/.htaccess';
-			if ( !file_exists( $htaccess_file ) ) {
-				file_put_contents( $htaccess_file, 'deny from all' );
-			}
+			// Verify that the uploads dir is available.
+			$this->verify_uploads_dir_ready();
 
 			// Delete old file if exists
 			$old_file = get_post_meta( $post_id, $this->meta_key, true );
@@ -307,8 +333,12 @@ class SimpleProtectedDownloads {
 			}
 
 			if ( isset( $_FILES['dcwdspd_file'] ) ) {
+				// Change the upload dir to be the custom dir.
 				add_filter( 'upload_dir', array( $this, 'change_upload_dir' ) );
+
 				$movefile = wp_handle_upload( $_FILES['dcwdspd_file'], array( 'test_form' => false ) );
+
+				// Remove the filter so other uploads are not redirected.
 				remove_filter( 'upload_dir', array( $this, 'change_upload_dir' ) );
 
 				if ( $movefile && ! isset( $movefile['error'] ) ) {
@@ -352,7 +382,7 @@ class SimpleProtectedDownloads {
 					
 					if ( $column === $this->meta_key ) {
 						$file_size = size_format( filesize( $file_path ) );
-						echo wp_sprintf( '%s<br><small style="color: #646970;">(%s)</small>', esc_html( $file_name), esc_html( $file_size ) );
+						echo wp_sprintf( '%s<br><small>(%s)</small>', esc_html( $file_name), esc_html( $file_size ) );
 						return;
 					}
 					if ( $column == $this->link_col ) {
@@ -362,7 +392,7 @@ class SimpleProtectedDownloads {
 					}
 				}
 			}
-			echo '<span style="color: #dba617;">—</span>';
+			echo '<span class="spd-no-file">—</span>';
 		}
 	}
 
@@ -371,11 +401,12 @@ class SimpleProtectedDownloads {
 	function add_download_url_copying_js() {
 		$screen = get_current_screen();
 		if ( 'edit-dcwd_simple_download' == $screen->id ) {
+			$click_colour = 'green';  // This is colour the link background temporarily changes to.
 ?>
 <style>
 .spd-copy-url { background-color: #2271b1; border-radius: 5px; color: #fff; cursor: pointer; padding: 5px; width: 100px; transition: all linear 0.3s; }
 .spd-copy-url.info-copied { background-color: green !important; scale: 1.1 !important; }
-.spd-copy-url.info-copied:after { content: 'Copied'; display: block; color: green; padding-top: 0.5em; font-size: 70%; font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif; }
+.spd-copy-url.info-copied:after { content: 'Copied'; display: block; color: <?php echo esc_attr( $click_colour ); ?>; padding-top: 0.5em; font-size: 70%; font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif; }
 </style>
 <script>
 jQuery(document).ready(function( $ ) {
@@ -386,7 +417,6 @@ jQuery(document).ready(function( $ ) {
 	$( '.spd-copy-url' ).on( 'click', function() {
 		spDownloadIcon = $(this);
 		spDownloadUrl = spDownloadIcon.data('spd_url');
-		//console.log( 'Download url', spDownloadUrl );
 
 		// Add the url to the clipboard.
 		navigator.clipboard.writeText(spDownloadUrl).then(function() {
@@ -403,4 +433,5 @@ jQuery(document).ready(function( $ ) {
 	}
 }
 
+//WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
 $SimpleProtectedDownloads = new SimpleProtectedDownloads();
